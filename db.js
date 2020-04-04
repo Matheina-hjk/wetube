df2ab1414ba21def952b2a402ea73a4fbe2923e5
Matheina-hjk <62123021+Matheina-hjk@users.noreply.github.com>
2020-04-05 02:42:25 +0900

    update

---
+++ db.js
@@ -0,0 +1,54 @@
+export const videos = [
+  {
+    id: 2345,
+    title: "Video awesome",
+    description: "This is something I love",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 1234,
+      name: "Hyejin",
+      email: "hyejinkim1207@naver.com",
+    },
+  },
+  {
+    id: 3456,
+    title: "Video Nice",
+    description: "This is something I love",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 7777,
+      name: "Hyejin",
+      email: "hyejinkim1207@naver.com",
+    },
+  },
+  {
+    id: 7890,
+    title: "Video Great",
+    description: "This is something I love",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 6543,
+      name: "Hyejin",
+      email: "hyejinkim1207@naver.com",
+    },
+  },
+  {
+    id: 6666,
+    title: "Video NUTS",
+    description: "This is something I love",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 3210,
+      name: "Hyejin",
+      email: "hyejinkim1207@naver.com",
+    },
+  },
+];
