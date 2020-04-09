be24ebc0177bdbe280bd0d2e7a8a897aeaad3f11
Matheina-hjk <62123021+Matheina-hjk@users.noreply.github.com>
2020-04-09 18:39:25 +0900

    Update videoController.js

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -58,13 +58,14 @@ export const getEditVideo = async (req, res) => {
     res.redirect(routes.home);
   }
 };
-export const postEditVideo = (req, res) => {
+export const postEditVideo = async (req, res) => {
   const {
     params: { id },
-    body:{title, description}
+    body: { title, description },
   } = req;
   try {
-    await Video.findOneAndUpdate({id},{title, description})
+    await Video.findOneAndUpdate({ id }, { title, description });
+    res.redirect(routes.videoDetail(id));
   } catch (error) {
     res.redirect(routes.home);
   }
