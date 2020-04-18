316ce580470eb332b488ca949826da25663944f7
Matheina-hjk <62123021+Matheina-hjk@users.noreply.github.com>
2020-04-18 17:22:57 +0900

    #4.2 Styles with Webpack part Two
    solved previous problem and get a new one
    omg

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,41 @@
 const path = require("path");
+const autoprefixer = require("autoprefixer");
 const ExtractCSS = require("extract-text-webpack-plugin");
 
-const EVN=process.env.WEBPACK_ENV;
+const MODE = process.env.WEBPACK_ENV;
 const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
 const OUTPUT_DIR = path.join(__dirname, "static");
 
 const config = {
   entry: ENTRY_FILE,
-  mode: MODE,
+  mode: MODE.replace(/\s/g, ""),
   module: {
-    rules:[
+    rules: [
       {
         test: /\.(scss)$/,
-        use: ExtractCSS.extract({
-          loader:"css-loader"
-        },
-        {
-          loader:"postcss-loader"
-        }
-        {
-          loader:"sass-loader"
-        }
-        )
-      }
-    ]
+        use: ExtractCSS.extract([
+          {
+            loader: "css-loader",
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              plugin() {
+                return [autoprefixer({ browsers: "cover 99.5%" })];
+              },
+            },
+          },
+          {
+            loader: "sass-loader",
+          },
+        ]),
+      },
+    ],
   },
   output: {
     path: OUTPUT_DIR,
-    filename: "[name].[format]",
+    filename: "[name].js",
   },
+  plugins: [new ExtractCSS("styles.css")],
 };
 module.exports = config;
