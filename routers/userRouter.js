b6c56811da192aa97717f96d97299bf8116f28ea
Matheina-hjk <62123021+Matheina-hjk@users.noreply.github.com>
2020-04-22 11:15:28 +0900

    7.2

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,11 +1,11 @@
 import express from "express";
 import routes from "../routes";
 import {
-  users,
   userDetail,
   getEditProfile,
-  changePassword,
+  getchangePassword,
   postEditProfile,
+  postChangePassword,
 } from "../controllers/userController";
 import { onlyPrivate, uploadAvatar } from "../middlewares";
 
@@ -14,7 +14,8 @@ const userRouter = express.Router();
 userRouter.get(routes.editProfile, onlyPrivate, getEditProfile);
 userRouter.post(routes.editProfile, onlyPrivate, uploadAvatar, postEditProfile);
 
-userRouter.get(routes.changePassword, onlyPrivate, changePassword);
+userRouter.get(routes.changePassword, onlyPrivate, getchangePassword);
+userRouter.post(routes.changePassword, onlyPrivate, postChangePassword);
 userRouter.get(routes.userDetail(), userDetail);
 
 export default userRouter;
